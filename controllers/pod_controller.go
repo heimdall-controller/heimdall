0670d4035dae958a6c3c21d07bb7d9375112d44c
Bryan Keane <bkeane@redhat.com>
2023-01-18 21:26:02 +0000

    HEIMCTR-12: Testing ci/cd

--- controllers/pod_controller.go
+++ controllers/pod_controller.go
@@ -35,15 +35,6 @@ type PodReconciler struct {
 //+kubebuilder:rbac:groups=meta.heimdall.wiki,resources=pods/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=meta.heimdall.wiki,resources=pods/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Pod object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
